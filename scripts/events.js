b130cf422a966c69eabcbad126262225b368b058
atul0030 <atul@knowdl.com>
2018-10-24 19:26:32 +0530

    update p25next page

--- scripts/events.js
+++ scripts/events.js
@@ -26,24 +26,7 @@ $(document).on("click", ".qheight", function (event) {
 var hotspotclicked = false;;
 var hotspot;
 $(document).on("click", ".divHotSpot", function (event) {
-    if(_Navigator.IsPresenterMode()){
-        return;
-    }
-    event.preventDefault();
-    $(this).k_disable()
-    if (hotspotclicked || _Navigator.IsAnswered())
-        return;
-    hotspotclicked = true;
-    $(this).addClass("hotspotclicked")
-    hotspot = $(this);
-    setTimeout(function () {
-        hotspotclicked = false;
-        _ModuleCommon.HotspotClick(hotspot, event);       
-    },400)
-    
-});
-$(document).on("dblclick touchend", ".divHotSpotDbClick", function (event) {
-    if(_Navigator.IsPresenterMode()){
+    if (_Navigator.IsPresenterMode()) {
         return;
     }
     event.preventDefault();
@@ -56,12 +39,29 @@ $(document).on("dblclick touchend", ".divHotSpotDbClick", function (event) {
     setTimeout(function () {
         hotspotclicked = false;
         _ModuleCommon.HotspotClick(hotspot, event);
-       
-    },400)
-    
+    }, 400)
+
+});
+$(document).on("dblclick touchend", ".divHotSpotDbClick", function (event) {
+    if (_Navigator.IsPresenterMode()) {
+        return;
+    }
+    event.preventDefault();
+    $(this).k_disable()
+    if (hotspotclicked || _Navigator.IsAnswered())
+        return;
+    hotspotclicked = true;
+    $(this).addClass("hotspotclicked")
+    hotspot = $(this);
+    setTimeout(function () {
+        hotspotclicked = false;
+        _ModuleCommon.HotspotClick(hotspot, event);
+
+    }, 400)
+
 });
 $(document).on("keydown", ".divHotSpotDbClick", function (event) {
-    if(_Navigator.IsPresenterMode()){
+    if (_Navigator.IsPresenterMode()) {
         return;
     }
     if (window.event) {
@@ -79,10 +79,12 @@ $(document).on("click", "#linkprevious", function (event) {
     _Navigator.Prev();
 });
 $(document).on("click", "#linknext", function (event) {
-    if ($(this).k_IsDisabled()) return;    
+    if ($(this).k_IsDisabled()) return;
     _Navigator.Next();
 });
 $(document).on("click", ".hintlink", function (event) {
+    if ($(this).parent().hasClass("disabled"))
+        return;
     if ($(this).hasClass("expanded")) {
         $(".hintlink").removeClass("expanded")
         $(".hintlink").attr("aria-expanded", "false")
@@ -133,25 +135,25 @@ $(document).on('click', ".reviewsubmit", function (event) {
     _Navigator.Next();
 });
 $(document).on('mouseover', ".hintlink", function (event) {
-    $(".hintlink .hintlinkspan").css({"color":"#b22222","border-bottom":"1px solid #b22222"})
-    $(this).find("path").css({"fill":"#b22222"})
+    $(".hintlink .hintlinkspan").css({ "color": "#b22222", "border-bottom": "1px solid #b22222" })
+    $(this).find("path").css({ "fill": "#b22222" })
 });
 
 $(document).on('mouseout', ".hintlink", function (event) {
- $(".hintlink .hintlinkspan").css({"color":"#047a9c","border-bottom":"1px solid #047a9c"})
- $(this).find("path").css({"fill":"#047a9c"}) 
+    $(".hintlink .hintlinkspan").css({ "color": "#047a9c", "border-bottom": "1px solid #047a9c" })
+    $(this).find("path").css({ "fill": "#047a9c" })
 });
 
 $(document).on("click", ".quizButton", function (event) {
-    _Navigator.LoadPage("p46") 
+    _Navigator.LoadPage("p46")
 });
 
 $(document).on("change", ".assessmentradio", function (event) {
-    $(".assessmentSubmit").k_enable();  
-  
+    $(".assessmentSubmit").k_enable();
+
 });
 $(document).on("click", ".assessmentSubmit", function (event) {
-    gRecordData.Questions[currentQuestionIndex].UserSelectedOptionId = $("input[type='radio']:checked").attr("id") ;
+    gRecordData.Questions[currentQuestionIndex].UserSelectedOptionId = $("input[type='radio']:checked").attr("id");
     gRecordData.Questions[currentQuestionIndex].IsAnswered = true;
     _Navigator.Next();
 });
@@ -160,32 +162,32 @@ $(document).on('click', "#continuebtn", function (event) {
 });
 $(document).on('click', ".buttonlink", function (event) {
     var buttonlink = [
-        { btnId: "button1", nextPageId: "p3",imageId:"tick1", lastpageId :["p8"]},
-        { btnId: "button2", nextPageId: "p14",imageId:"tick2",lastpageId : ["p23"]},
-        { btnId: "button3", nextPageId: "p33" ,imageId:"tick3",lastpageId :["p39"]},
-        { btnId: "button4", nextPageId: "p44" ,imageId:"tick4",lastpageId :["p44"]},
-        { btnId: "button5", nextPageId: "p9",imageId:"tick5",lastpageId : ["p13"]},
-        { btnId: "button6", nextPageId: "p25" ,imageId:"tick6",lastpageId :["p31","p32"]},
-        { btnId: "button7", nextPageId: "p40" ,imageId:"tick7",lastpageId :["p43"]},
-        { btnId: "button8", nextPageId: "p45" ,imageId:"tick8",lastpageId :["p45"]},
-      ]
+        { btnId: "button1", nextPageId: "p3", imageId: "tick1", lastpageId: ["p8"] },
+        { btnId: "button2", nextPageId: "p14", imageId: "tick2", lastpageId: ["p23"] },
+        { btnId: "button3", nextPageId: "p33", imageId: "tick3", lastpageId: ["p39"] },
+        { btnId: "button4", nextPageId: "p44", imageId: "tick4", lastpageId: ["p44"] },
+        { btnId: "button5", nextPageId: "p9", imageId: "tick5", lastpageId: ["p13"] },
+        { btnId: "button6", nextPageId: "p25", imageId: "tick6", lastpageId: ["p31", "p32"] },
+        { btnId: "button7", nextPageId: "p40", imageId: "tick7", lastpageId: ["p43"] },
+        { btnId: "button8", nextPageId: "p45", imageId: "tick8", lastpageId: ["p45"] },
+    ]
     var btnId = $(this).attr("id");
     var nextPageId = "";
     if (btnId != undefined) {
         for (var i = 0; i < buttonlink.length; i++) {
             if (buttonlink[i].btnId == btnId) {
                 nextPageId = buttonlink[i].nextPageId;
-                
+
                 break;
             }
         }
         if (nextPageId != "" && nextPageId != undefined) {
             // _Navigator.Next(); 
-            _Navigator.LoadPage(nextPageId);              
+            _Navigator.LoadPage(nextPageId);
         }
     }
 });
 
 $(document).on('click', ".inputcircle", function (event) {
-    $(this).next(".inpputtext").trigger( "click" );
-    });
+    $(this).next(".inpputtext").trigger("click");
+});
