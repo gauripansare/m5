4620890ad84a05e053eaa518f55a8f91cf48a1be
atul0030 <atul@knowdl.com>
2018-12-04 17:32:55 +0530

    update file

--- scripts/navigator.js
+++ scripts/navigator.js
@@ -858,7 +858,7 @@ var _Navigator = (function () {
             var bookmarkobj = {}
             bookmarkobj.BMPageId = bookmarkpageid;
             bookmarkobj.BMretrycnt = retrycnt;
-            bookmarkobj.BMg_RuntimeData = _ModuleCommon.Getg_RuntimeData();
+            //bookmarkobj.BMg_RuntimeData = _ModuleCommon.Getg_RuntimeData();
             bookmarkobj.VisistedPages = this.GetNavigatorBMData();
             bookmarkobj.ProgressLevels = progressLevels;
             bookmarkobj.ReviewData = _ModuleCommon.GetReviewData();
@@ -879,7 +879,7 @@ var _Navigator = (function () {
             var gVisistedPages = [];
             for (var i in _NData) {
                 if (_NData[i].isAnswered) {
-                    gVisistedPages.push(_NData[i].pageId)
+                    gVisistedPages.push({ id: _NData[i].pageId, prev: _NData[i].prevPageId, next: _NData[i].nextPageId });
                 }
             }
             return gVisistedPages;
@@ -887,7 +887,9 @@ var _Navigator = (function () {
         SetNavigatorBMData: function (gVisistedPages) {
 
             for (var i = 0; i < gVisistedPages.length; i++) {
-                _NData[gVisistedPages[i]].isAnswered = true;
+                _NData[gVisistedPages[i].id].isAnswered = true;
+                _NData[gVisistedPages[i].id].prevPageId = gVisistedPages[i].prev;
+                _NData[gVisistedPages[i].id].nextPageId = gVisistedPages[i].next;
             }
         },
 
@@ -915,7 +917,7 @@ var _Navigator = (function () {
                 bookmarkdata = JSON.parse(bookmarkdata);
                 bookmarkpageid = bookmarkdata.BMPageId;
                 retrycnt = bookmarkdata.BMretrycnt;
-                _ModuleCommon.Setg_RuntimeData(bookmarkdata.BMg_RuntimeData);
+                //_ModuleCommon.Setg_RuntimeData(bookmarkdata.BMg_RuntimeData);
                 this.SetNavigatorBMData(bookmarkdata.VisistedPages)
                 progressLevels = bookmarkdata.ProgressLevels;
                 _ModuleCommon.SetReviewData(bookmarkdata.ReviewData)
